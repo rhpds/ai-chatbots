3fab21c85af0c104068e52c6ded884faefb91e8d
Tony Kay <tony.g.kay@gmail.com>
2024-03-12 10:20:23 -0600

    Update chatbot to use ChatOllama model

--- chatbot-llm.py
+++ chatbot-llm.py
@@ -1,5 +1,4 @@
-# from langchain.chat_models import ChatOpenAI
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
 
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
@@ -7,16 +6,28 @@ from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 
 import chainlit as cl
+import config as cfg
+import os
+
+# TODO: Move these to config?
+OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", cfg.OLLAMA_BASE_URL)
+OLLAMA_MODEL = "mistral"
 
 
 @cl.on_chat_start
 async def on_chat_start():
-    model = ChatOpenAI(streaming=True)
+    model = ChatOllama(
+        model=OLLAMA_MODEL,
+        base_url=OLLAMA_BASE_URL,
+        streaming=True,
+    )
+
+    # TODO: Refacor to use ChatPromptTemplate.from_template in a prompt.py
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+                "You're a very helpful Software Development and Technology Assistant.",
             ),
             ("human", "{question}"),
         ]
